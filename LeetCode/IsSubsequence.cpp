90c36b0a786ccf6433eca812116b8464d4604db0
Sarthak Sharma <sarthak753@gmail.com>
2024-01-22 02:07:20 +0530

    IsSubsequenceUpdated

---
+++ LeetCode/IsSubsequence.cpp
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/is-subsequence/?envType=study-plan-v2&envId=leetcode-75
+
+// Example 1:
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
+
+// Example 2:
+// Input: s = "axc", t = "ahbgdc"
+// Output: false
+
+// Order of string1 is important
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m = s.size(), n = t.size();
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (s[i] == t[j]) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return (i == m) ? true : false;
+    }
+};
+// class Solution {
+// public:
+//     bool isSubsequence(string s, string t) {
+//                 int n = s.length(),m=t.length();
+//         int j = 0; 
+//         // For index of s (or subsequence
+    
+//         // Traverse s and t, and
+//         // compare current character
+//         // of s with first unmatched char
+//         // of t, if matched
+//         // then move ahead in s
+//         for (int i = 0; i < m and j < n; i++)
+//             if (s[j] == t[i])
+//                 j++;
+    
+//         // If all characters of s were found in t
+//         return (j == n);
+//         }
+
+// };
