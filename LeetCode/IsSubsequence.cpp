c3cc7522a8de04e75908d5abbea0bd82be15b53b
Sarthak Sharma <sarthak753@gmail.com>
2024-01-22 20:27:35 +0530

    NumberofRecentCalls

--- LeetCode/IsSubsequence.cpp
+++ LeetCode/IsSubsequence.cpp
@@ -49,3 +49,30 @@ public:
 //         }
 
 // };
+
+// // If ordering is not important, We can use unordered_set
+// #include<bits/stdc++.h>
+// using namespace std;
+ 
+// class Solution {
+// public:
+//     bool isSubsequence(string s, string t) {
+//         int c=0;
+//         unordered_set<char> s1;
+//         for(int i=0;i<s.size();i++){
+//             s1.insert(s[i]);
+//         }
+//         unordered_set<char> s2(s1.begin(), s1.end());
+//         for(int i=0;i<t.size();i++){
+//             if(s1.find(t[i])!=s1.end()){
+//                 s1.erase(t[i]);
+//             }
+//         }
+//         // for(auto itr:s2) cout<<itr<<" 2 "<<endl;
+//         // for(auto itr:s1) cout<<itr<<" ";
+
+//         if(s1.empty()) c=1;
+//         return c;
+//     }
+
+// };
