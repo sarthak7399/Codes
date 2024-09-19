d27bd7049511e323b6f639c197e2e84523cfa01e
Sarthak <sarthak753@gmail.com>
2024-09-19 18:04:07 +0530

    Different Ways to Add Parentheses

---
+++ Python/Different Ways to Add Parentheses.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/different-ways-to-add-parentheses/
+
+# Example 1:
+# Input: expression = "2-1-1"
+# Output: [0,2]
+# Explanation:
+# ((2-1)-1) = 0 
+# (2-(1-1)) = 2
+
+from typing import List
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        res = []
+        # ans = []
+        for i in range(len(expression)):
+            oper = expression[i]
+            if oper == "+" or oper == "-" or oper == "*":
+                sub_str1 = expression[0 : i]
+                sub_str2 = expression[i + 1 : ]
+                s1 = self.diffWaysToCompute(sub_str1)
+                s2 = self.diffWaysToCompute(sub_str2)
+                for i in s1:
+                    for j in s2:
+                        if oper == "*":
+                            res.append(int(i) * int(j))
+                        if oper == "+":
+                            res.append(int(i) + int(j))
+                        if oper == "-":
+                            res.append(int(i) - int(j))
+        if len(res) == 0:
+            res.append(int(expression))
+        # print(res)
+        return res
