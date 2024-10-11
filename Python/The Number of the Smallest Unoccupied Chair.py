e1128e3686b5c9a76b759533a902d29ce8e81785
Sarthak <sarthak753@gmail.com>
2024-10-12 00:00:45 +0530

    The Number of the Smallest Unoccupied Chair

---
+++ Python/The Number of the Smallest Unoccupied Chair.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/the-number-of-the-smallest-unoccupied-chair/
+
+# Example 1:
+# Input: times = [[1,4],[2,3],[4,6]], targetFriend = 1
+# Output: 1
+# Explanation: 
+# - Friend 0 arrives at time 1 and sits on chair 0.
+# - Friend 1 arrives at time 2 and sits on chair 1.
+# - Friend 1 leaves at time 3 and chair 1 becomes empty.
+# - Friend 0 leaves at time 4 and chair 0 becomes empty.
+# - Friend 2 arrives at time 4 and sits on chair 0.
+# Since friend 1 sat on chair 1, we return 1.
+
+from heapq import heappop, heappush
+from typing import List
+class Solution:
+    def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
+
+        order = sorted(range(len(times)), key = lambda x: times[x][0])  # <-- 1)
+        emptySeats, takenSeats = list(range(len(times))), []            # <-- 2)
+
+        for i in order:                                                 # <-- 3)
+            ar, lv = times[i]
+
+            while takenSeats and takenSeats[0][0] <= ar:
+                heappush(emptySeats, heappop(takenSeats)[1])
+            seat = heappop(emptySeats)                                  # <-- 4)
+
+            if i == targetFriend: return seat
+
+            heappush(takenSeats,(lv, seat))                             # <-- 5)
