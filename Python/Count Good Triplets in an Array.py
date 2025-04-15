1875b12fa69d39d47a1e9f693b57939ed02a67ae
sarthak7399 <sarthak753@gmail.com>
2025-04-15 23:18:52 +0530

    Count Good Triplets in an Array

---
+++ Python/Count Good Triplets in an Array.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/count-good-triplets-in-an-array/
+
+# Example 1:
+# Input: nums1 = [2,0,1,3], nums2 = [0,1,2,3]
+# Output: 1
+# Explanation: 
+# There are 4 triplets (x,y,z) such that pos1x < pos1y < pos1z. They are (2,0,1), (2,0,3), (2,1,3), and (0,1,3). 
+# Out of those triplets, only the triplet (0,1,3) satisfies pos2x < pos2y < pos2z. Hence, there is only 1 good triplet.
+
