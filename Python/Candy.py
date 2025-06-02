8587d1f34c0303bcebf71fbbf232a19af4988459
sarthak7399 <sarthak753@gmail.com>
2025-06-02 23:16:56 +0530

    Candy

---
+++ Python/Candy.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/candy/
+
+# Example 1:
+# Input: ratings = [1,0,2]
+# Output: 5
+# Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
+
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        n = len(ratings)
+        candies = [1] * n  # Step 1: Give each child 1 candy initially
+
+        # Step 2: Left to Right pass
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        # Step 3: Right to Left pass
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+
+        # Step 4: Return total candies
+        return sum(candies)
