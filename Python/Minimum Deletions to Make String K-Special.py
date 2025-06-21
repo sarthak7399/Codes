6b04a2885dee323b1445e75a1a18298dd8236983
sarthak7399 <sarthak753@gmail.com>
2025-06-21 21:34:29 +0530

    Minimum Deletions to Make String K-Special

---
+++ Python/Minimum Deletions to Make String K-Special.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/minimum-deletions-to-make-string-k-special/
+
+# Example 1:
+# Input: word = "aabcaba", k = 0
+# Output: 3
+# Explanation: We can make word 0-special by deleting 2 occurrences of "a" and 1 occurrence of "c". Therefore, word becomes equal to "baba" where freq('a') == freq('b') == 2.
+
