8f9da6543f916dd92ee2108c809915fe9cd3a6e3
sarthak7399 <sarthak753@gmail.com>
2025-07-26 00:49:14 +0530

    Maximum Unique Subarray Sum After Deletion

---
+++ Python/Maximum Unique Subarray Sum After Deletion.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/maximum-unique-subarray-sum-after-deletion/
+
+# Example 1:
+# Input: nums = [1,2,3,4,5]
+# Output: 15
+# Explanation:
+# Select the entire array without deleting any element to obtain the maximum sum.
+
