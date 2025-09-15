0f5ebb8b2fde636e9ec95fe890831ac41b664de7
sarthak7399 <sarthak753@gmail.com>
2025-09-16 00:31:22 +0530

    Maximum Number of Words You Can Type

---
+++ Python/Maximum Number of Words You Can Type.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/maximum-number-of-words-you-can-type/
+
+# Example 1:
+# Input: text = "hello world", brokenLetters = "ad"
+# Output: 1
+# Explanation: We cannot type "world" because the 'd' key is broken.
+
