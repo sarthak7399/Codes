0ba9777dd302dc73b8706289615ca45cace9a089
sarthak7399 <sarthak753@gmail.com>
2025-11-23 20:35:37 +0530

    Greatest Sum Divisible by Three

---
+++ Python/Greatest Sum Divisible by Three.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/greatest-sum-divisible-by-three/
+
+# Example 1:
+# Input: nums = [3,6,5,1,8]
+# Output: 18
+# Explanation: Pick numbers 3, 6, 1 and 8 their sum is 18 (maximum sum divisible by 3).
+
