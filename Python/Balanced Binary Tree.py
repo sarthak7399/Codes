7e93edf95777795f68f4882075601bae64601804
sarthak7399 <sarthak753@gmail.com>
2026-02-27 02:05:47 +0530

    Balanced Binary Tree

--- Python/Balanced Binary Tree.py
+++ Python/Balanced Binary Tree.py
@@ -4,6 +4,12 @@
 # Input: root = [1,2,2,3,3,null,null,4,4]
 # Output: false
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def isBalanced(self, root: TreeNode) -> bool:
         return self.height(root) != -1
