525cb01ee4793ed8186701ca1a210b2a41627149
sarthak7399 <sarthak753@gmail.com>
2026-02-27 02:06:07 +0530

    Binary Watch

--- Python/Binary Watch.py
+++ Python/Binary Watch.py
@@ -4,6 +4,8 @@
 # Input: turnedOn = 1
 # Output: ["0:01","0:02","0:04","0:08","0:16","0:32","1:00","2:00","4:00","8:00"]
 
+from typing import List
+
 class Solution:
     def readBinaryWatch(self, k: int) -> List[str]:
         # If no LEDs are ON → only possible time
