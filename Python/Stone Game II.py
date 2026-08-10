959146ca3efba2d4af9a77d9534e4181c062a7c7
sarthak7399 <sarthak753@gmail.com>
2026-08-10 23:11:59 +0530

    Stone Game II

--- Python/Stone Game II.py
+++ Python/Stone Game II.py
@@ -5,6 +5,8 @@
 # Output: 10
 # Explanation:  If Alice takes one pile at the beginning, Bob takes two piles, then Alice takes 2 piles again. Alice can get 2 + 4 + 4 = 10 piles in total. If Alice takes two piles at the beginning, then Bob can take all three piles left. In this case, Alice get 2 + 7 = 9 piles in total. So we return 10 since it's larger. 
 
+from typing import List
+
 class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
         # Number of piles
